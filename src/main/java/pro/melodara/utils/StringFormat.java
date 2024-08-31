41f8ac25eac68ab72c0be44771823b8865af8943
Alim <zhaba.v.kedah77@gmail.com>
2024-09-01 02:08:58 +0500

    string utils

---
+++ src/main/java/pro/melodara/utils/StringFormat.java
@@ -0,0 +1,32 @@
+package pro.melodara.utils;
+
+import java.time.Duration;
+
+public class StringFormat {
+    public static String limitString(int maxCharsCount, String string) {
+        if (string.length() > maxCharsCount) {
+            return string.substring(0, maxCharsCount - 3) + "...";
+        }
+        return string;
+    }
+
+    public static String getDurationWithNames(long duration) {
+        Duration d = Duration.ofMillis(duration);
+        long hours = d.toHours();
+        long minutes = d.toMinutes() % 60;
+        long seconds = d.getSeconds() % 60;
+
+        return (hours == 0 ? "" : (hours + " hrs ")) +
+                (minutes == 0 ? "" : (minutes + " mins ")) +
+                seconds + " secs";
+    }
+
+    public static String getDuration(long duration) {
+        Duration d = Duration.ofMillis(duration);
+        long hours = d.toHours();
+        long minutes = d.toMinutes() % 60;
+        long seconds = d.getSeconds() % 60;
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+}
