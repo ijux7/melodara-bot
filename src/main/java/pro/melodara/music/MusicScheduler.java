212fb6d1ec4c5cf4f7cad07826beb2a5ae5b07de
SilverFox <vereskont254@gmail.com>
2024-09-15 12:31:20 +0300

    Fix fucking bug

--- src/main/java/pro/melodara/music/MusicScheduler.java
+++ src/main/java/pro/melodara/music/MusicScheduler.java
@@ -2,13 +2,13 @@ package pro.melodara.music;
 
 import dev.arbjerg.lavalink.client.player.Track;
 import dev.arbjerg.lavalink.protocol.v4.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class MusicScheduler {
+    private static final Logger log = LoggerFactory.getLogger(MusicScheduler.class);
     private final MusicManager manager;
     private final Queue<Track> nextQueue = new LinkedList<>();
     private final Queue<Track> previousQueue = new LinkedList<>();
@@ -64,15 +64,15 @@ public class MusicScheduler {
         List<Track> TEMP = new LinkedList<>(previousQueue);
         TEMP.add(0, track);
         previousQueue.clear();
-        TEMP.forEach(previousQueue::offer);
+        previousQueue.addAll(TEMP);
     }
 
     private void startTrack(Track track) {
-        manager.getLink().ifPresent(
+       manager.getLink().ifPresent(
                 link -> link.createOrUpdatePlayer()
                         .setTrack(track)
                         .setVolume(100) // default 100
-                        .subscribe(s -> manager.getMusicMessage().sendMessageWhenStarts(), f -> playNextTrack())
+                        .subscribe(s -> manager.getMusicMessage().sendMessageWhenStarts(), Throwable::printStackTrace)
         );
     }
 
@@ -148,13 +148,16 @@ public class MusicScheduler {
 
     // events
 
+    /**
+     * @apiNote lastTrack returning object of ENDED track (with ending position)
+     */
     public void onTrackEnd(Track lastTrack, Message.EmittedEvent.TrackEndEvent.AudioTrackEndReason endReason) {
         if (endReason.getMayStartNext()) {
             if (getRepeatType().equals(RepeatType.TRACK)) {
-                startTrack(lastTrack);
+                startTrack(this.currentTrack);
             } else if (getRepeatType().equals(RepeatType.QUEUE) && nextQueue.isEmpty()) {
-                enqueueInPreviousQueueAsZero(lastTrack);
-                List<Track> queue = getPreviousQueue();
+                enqueueInPreviousQueueAsZero(this.currentTrack);
+                List<Track> queue = new ArrayList<>(getPreviousQueue());
                 previousQueue.clear();
                 Collections.reverse(queue);
 
